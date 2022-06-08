b34688276566e5e303464197bb26522da37cc8f6
June Kim <zhxxne@gmail.com>
2022-06-08 20:07:00 +0200

    add habit component, style and font

--- habit-tracker/src/app.jsx
+++ habit-tracker/src/app.jsx
@@ -1,23 +1,10 @@
 import React from "react";
 import './app.css';
+import Habit from "./components/habit";
 
 function App() {
-  const name = 'World'
   return (
-    <>
-      <h1>Hello {name}!</h1>
-      <h1>Welcome</h1>
-      {name && <h1> Hello {name}!</h1>}
-      {
-        ['🍔', '🍟'].map(item => (
-          <h1>{item}</h1>
-        ))
-      }
-    </>
-    // <React.Fragment>
-    //   <h1>Hello {name}!</h1>
-    //   <h1>Welcome</h1>
-    // </React.Fragment>
+    <Habit />
   );
 }
 
