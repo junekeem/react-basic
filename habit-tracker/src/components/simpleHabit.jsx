5af0e632fac1376c1ce63a9cd7e6d2e664bf405e
June Kim <zhxxne@gmail.com>
2022-08-19 21:31:46 +0200

    update simple habit class to react hook

--- habit-tracker/src/components/simpleHabit.jsx
+++ habit-tracker/src/components/simpleHabit.jsx
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
-class SimpleHabit extends Component {
-  state = {
-    count: 0,
-  };
+const SimpleHabit = (props) => {
+  // Class:
+  // state = {
+  //   count: 0,
+  // };
+  const [count, setCount] = useState(0);
+  const spanRef = useRef();
 
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+  useEffect(() => {
+    console.log(`mounted & updated!: ${count}`);
+  }, [count]);
 
-  render() {
-    return (
-      <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
-        <button
-          className="habit-button habit-increase"
-          onClick={this.handleIncrement}
-        >
-          <i className="fas fa-plus-square"></i>
-        </button>
-      </li>
-    );
-  }
-}
+  const handleIncrement = useCallback(() => {
+    setCount(count + 1);
+  });
+  return (
+    <li className="habit">
+      <span ref={spanRef} className="habit-name">Reading</span>
+      <span className="habit-count">{count}</span>
+      <button
+        className="habit-button habit-increase"
+        onClick={handleIncrement}
+      >
+        <i className="fas fa-plus-square"></i>
+      </button>
+    </li>
+  );
+};
 
 export default SimpleHabit;
