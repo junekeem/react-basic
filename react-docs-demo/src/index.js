30015fbdba5b57b5047e1c5c682fc08bb4e1fd5a
June Kim <zhxxne@gmail.com>
2022-10-28 14:00:35 +0200

    rendering elements

--- react-docs-demo/src/index.js
+++ react-docs-demo/src/index.js
@@ -4,8 +4,16 @@ import './index.css';
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function tick() {
+  const element = (<h2>It is {new Date().toLocaleTimeString()}</h2>);
+  root.render(element);
+}
+
+setInterval(tick, 1000);
+
+// root.render(
+//   <React.StrictMode>
+//     <App/>
+//   </React.StrictMode>
+// );
