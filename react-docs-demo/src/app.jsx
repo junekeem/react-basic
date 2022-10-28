40c207659342965212f172f047d2e6e9c8882c70
June Kim <zhxxne@gmail.com>
2022-10-28 11:30:24 +0200

    introducing JSX

--- react-docs-demo/src/app.jsx
+++ react-docs-demo/src/app.jsx
@@ -1,8 +1,28 @@
 import './app.css';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Max',
+  lastName: 'Mustermann'
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <h1>Hello, {formatName(user)}</h1>
+      {getGreeting(user)}
+    </>
   );
 }
 
