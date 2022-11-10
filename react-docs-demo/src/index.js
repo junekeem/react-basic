d839ce97f110bcf0174ea32863f6dbe28cc4cf4b
June Kim <zhxxne@gmail.com>
2022-11-10 18:16:52 +0100

    components and props

--- react-docs-demo/src/index.js
+++ react-docs-demo/src/index.js
@@ -5,15 +5,8 @@ import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function tick() {
-  const element = (<h2>It is {new Date().toLocaleTimeString()}</h2>);
-  root.render(element);
-}
-
-setInterval(tick, 1000);
-
-// root.render(
-//   <React.StrictMode>
-//     <App/>
-//   </React.StrictMode>
-// );
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
