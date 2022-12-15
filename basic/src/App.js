73d10a9809a43f439a9a7b1c36f321853181d94a
June Kim <zhxxne@gmail.com>
2022-12-15 11:33:06 +0100

    [basic] simple list in App.js

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "user";
+  const list = ['apple', 'banana', 'cranberry'];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello! {name}</h1>
+      <h1>{`Hello! ${name}`}</h1>
+
+      <ul>
+        {list.map((item) =>
+          <li>{item}</li>
+        )}
+      </ul>
+    </>
   );
 }
 
