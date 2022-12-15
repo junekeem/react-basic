5bfa753ea95f2c55b19afa06faf33e9040954215
June Kim <zhxxne@gmail.com>
2022-12-15 17:33:39 +0100

    [basic] avatar

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,8 +1,9 @@
+import Avatar from "./Avatar";
+
 export default function Profile({ image, name, title, isNew }) {
   return (
     <div className='profile'>
-      <img className='photo' src={image} alt='avatar'/>
-      {isNew && <span className='new'>NEW</span>}
+      <Avatar image={image} isNew={isNew}></Avatar>
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
