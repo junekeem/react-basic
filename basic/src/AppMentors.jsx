75ccc9744f137f5c607c86ff01845851250ed429
June Kim <zhxxne@gmail.com>
2022-12-23 11:25:47 +0100

    [deepdive] practice 4

--- basic/src/AppMentors.jsx
+++ basic/src/AppMentors.jsx
@@ -31,12 +31,12 @@ export default function AppMentors() {
         onClick={() => {
           const prev = prompt(`Choose the name you want to change`);
           const current = prompt(`Enter the new name`);
-/*          const index = person.mentors.findIndex(mentor => (mentor.name === prev));
-          let mentors = person.mentors;
-          if (index !== -1) {
-            mentors[index] = { ...mentors[index], name: current };
-          }
-          setPerson(person => ({ ...person, mentors }))*/
+          /*          const index = person.mentors.findIndex(mentor => (mentor.name === prev));
+                    let mentors = person.mentors;
+                    if (index !== -1) {
+                      mentors[index] = { ...mentors[index], name: current };
+                    }
+                    setPerson(person => ({ ...person, mentors }))*/
 
           setPerson(person => ({
             ...person, mentors: person.mentors.map(mentor => {
@@ -49,6 +49,25 @@ export default function AppMentors() {
         }}>
         멘토의 이름을 바꾸기
       < /button>
+      <button
+        onClick={() => {
+          const name = prompt(`Enter the new mentor's name`);
+          const title = prompt(`Enter the new mentor's title`);
+          setPerson({ ...person, mentors: [...person.mentors, { name, title }] })
+        }}
+      >추가
+      </button>
+      <button
+        onClick={() => {
+          const name = prompt(`Enter the mentor's name to delete`);
+          setPerson({
+            ...person, mentors: person.mentors.filter(mentor => {
+              return mentor.name !== name;
+            })
+          })
+        }}
+      >삭제
+      </button>
     </div>
   );
 }
