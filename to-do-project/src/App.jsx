76feb62c1adad6c190e91567522111498112f43a
June Kim <zhxxne@gmail.com>
2023-01-27 21:18:25 +0100

    [to-do-project] darkmode

--- to-do-project/src/App.jsx
+++ to-do-project/src/App.jsx
@@ -2,16 +2,18 @@ import './App.css';
 import Header from "./components/Header/Header";
 import TaskList from "./components/TaskList/TaskList";
 import { useState } from "react";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ['all', 'active', 'completed']
 
 function App() {
   const [filter, setFilter] = useState(filters[0]);
+
   return (
-    <>
+    <DarkModeProvider>
       <Header filters={filters} filter={filter} onFilterChange={setFilter}/>
       <TaskList filter={filter}/>
-    </>
+    </DarkModeProvider>
   );
 }
 
--- to-do-project/src/components/Header/Header.jsx
+++ to-do-project/src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
 import styles from './Header.module.css';
+import { BsSunFill, BsMoonFill } from "react-icons/bs";
+import { useContext } from "react";
+import { useDarkMode } from "../../context/DarkModeContext";
+
 
 export default function Header({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
 
   return (
     <header className={styles.header}>
+      <button className={styles.toggle} onClick={toggleDarkMode}>
+        {/*{darkMode ? <BsSunFill/> : <BsMoonFill/>}*/}
+        {!darkMode && <BsMoonFill />}
+        {darkMode && <BsSunFill />}
+      </button>
       <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
